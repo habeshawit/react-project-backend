4ff863c436a8b6c3faddbfd1b84444e020c51e4d
habeshawit <shalomseleshi@gmail.com>
2021-05-17 11:44:35 -0400

    ability to create, see and destroy categories added

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ gem 'puma', '~> 5.0'
 gem 'rack-cors'
 gem 'active_model_serializers'
 gem 'fast_jsonapi'
+gem 'pry'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -5,4 +5,51 @@ class Api::V1::CategoriesController < ApplicationController
       # render json: CategorySerializer.new(categories)
       render json: categories
     end
+
+    def create
+      category = Category.new(category_params)
+      
+      if category.save   
+          render json: category        
+      else
+          render json: {errors: 'Error creating category'}
+      end
+  end
+
+  def show 
+      category = Category.find_by(id: params[:id])
+
+      if category 
+          render json: category
+      else 
+          render json: {message: 'Category not found.' }
+      end 
+  end 
+
+  # def update
+  #     item = item.find_by_id(params[:id])
+  #     item.name = params[:name]
+  #     item.description = params[:description]
+  #     item.price = params[:price]
+  #     item.image_url = params[:image_url]
+  #     item.category_id = params[:category_id]
+
+  #     if item.save
+  #         render json: ItemSerializer.new(item), status: :accepted
+  #     else
+  #         render json: {errors: item.errors.full_message}, status: :unprocessable_entity
+  #     end
+  # end
+
+  def destroy
+      category = Category.find(params[:id])
+      category.destroy
+  end
+
+  private
+
+  def category_params
+      params.require(:category).permit(:name)
+  end
+
 end
